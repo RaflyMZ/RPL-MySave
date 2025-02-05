051a41da160ad812447fbbf01b59ab3e2a60e872
Badharalfath <94126104+Badharalfath@users.noreply.github.com>
2025-02-05 15:02:55 +0700

    add login and register

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,15 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\FinansialController;
 use App\Http\Controllers\GuideController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 // Route untuk halaman Home
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
 
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
 Route::get('/editProfile', [ProfileController::class, 'edit'])->name('editProfile');
